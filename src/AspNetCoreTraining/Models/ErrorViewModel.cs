288eab9d574aab39db3dba041d3b588b17861a97
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-23 10:10:20 +0100

    MVC project created

---
+++ src/AspNetCoreTraining/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreTraining.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
