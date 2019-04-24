1dea37612d67c22ce6e4ad97493f9f1bae36a881
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-24 10:25:26 +0100

    Manage users index view created.

--- src/AspNetCoreTraining/Controllers/ManageUsersController.cs
+++ src/AspNetCoreTraining/Controllers/ManageUsersController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AspNetCoreTraining.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AspNetCoreTraining.Controllers
 {
@@ -12,6 +16,20 @@ namespace AspNetCoreTraining.Controllers
         public ManageUsersController(UserManager<IdentityUser> userManager)
         {
             this._userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var administrators = (await this._userManager.GetUsersInRoleAsync("Administrator")).ToArray();
+            var everyone = await this._userManager.Users.ToArrayAsync();
+
+            var model = new ManagerUsersViewModel()
+            {
+                Administrators = administrators,
+                Everyone = everyone
+            };
+
+            return this.View(model);
         }
     }
 }
---
+++ src/AspNetCoreTraining/Models/ViewModel/ManagerUsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTraining.Models.ViewModel
+{
+    public class ManagerUsersViewModel
+    {
+        public IdentityUser[] Administrators { get; set; }
+        public IdentityUser[] Everyone { get; set; }
+    }
+}
