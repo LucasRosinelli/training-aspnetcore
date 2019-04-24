3900ef469a3064833907794ea2c660c1da36dc0d
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-24 17:53:46 +0100

    To-Do Item Service Should - Add Item

---
+++ test/AspNetCoreTraining.UnitTests/ToDoItemServiceShould.cs
@@ -0,0 +1,47 @@
+﻿using AspNetCoreTraining.Data;
+using AspNetCoreTraining.Models.Dto;
+using AspNetCoreTraining.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreTraining.UnitTests
+{
+    public class ToDoItemServiceShould
+    {
+        [Fact]
+        public async Task AddItemAsIncomplete()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser = new IdentityUser()
+                {
+                    Id = "fake-000",
+                    UserName = "fake@training.local"
+                };
+
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Is text working?"
+                }, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context.Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Is text working?", item.Title);
+                Assert.False(item.IsDone);
+                Assert.Null(item.DueAt);
+            }
+        }
+    }
+}
