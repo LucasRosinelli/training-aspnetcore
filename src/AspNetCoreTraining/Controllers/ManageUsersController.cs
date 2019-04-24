d09556ef77d66d11916110ad239d62268dcd1e01
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-24 10:14:23 +0100

    Manage users controller created.

---
+++ src/AspNetCoreTraining/Controllers/ManageUsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTraining.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ManageUsersController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+    }
+}
