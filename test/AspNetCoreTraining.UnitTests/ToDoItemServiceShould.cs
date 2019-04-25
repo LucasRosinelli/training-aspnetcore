626812e55161290865a62a82317dc8cfb09f6cb5
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-25 09:07:25 +0100

    Get incomplete test added.

--- test/AspNetCoreTraining.UnitTests/ToDoItemServiceShould.cs
+++ test/AspNetCoreTraining.UnitTests/ToDoItemServiceShould.cs
@@ -43,5 +43,90 @@ namespace AspNetCoreTraining.UnitTests
                 Assert.Null(item.DueAt);
             }
         }
+
+        [Fact]
+        public async Task GetIncompleteItemsAssert()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestAddItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser1 = new IdentityUser()
+                {
+                    Id = "fake-001",
+                    UserName = "fake1@training.local"
+                };
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 1 - Item 1 (completed)"
+                }, fakeUser1);
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 1 - Item 2"
+                }, fakeUser1);
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 1 - Item 3"
+                }, fakeUser1);
+                var fake1Items = await service.GetIncompleteItemsAsync(fakeUser1);
+                await service.MarkDoneAsync(fake1Items[0].Id, fakeUser1);
+
+                var fakeUser2 = new IdentityUser()
+                {
+                    Id = "fake-002",
+                    UserName = "fake2@training.local"
+                };
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 2 - Item 1"
+                }, fakeUser2);
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 2 - Item 2 (completed)"
+                }, fakeUser2);
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 2 - Item 3"
+                }, fakeUser2);
+                await service.AddItemAsync(new AddToDoItem()
+                {
+                    Title = "Fake 2 - Item 4"
+                }, fakeUser2);
+                var fake2Items = await service.GetIncompleteItemsAsync(fakeUser2);
+                await service.MarkDoneAsync(fake2Items[1].Id, fakeUser2);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser1 = new IdentityUser()
+                {
+                    Id = "fake-001",
+                    UserName = "fake1@training.local"
+                };
+                var fake1Items = await service.GetIncompleteItemsAsync(fakeUser1);
+                Assert.Equal(2, fake1Items.Length);
+
+                var fakeUser2 = new IdentityUser()
+                {
+                    Id = "fake-002",
+                    UserName = "fake2@training.local"
+                };
+                var fake2Items = await service.GetIncompleteItemsAsync(fakeUser2);
+                Assert.Equal(3, fake2Items.Length);
+
+                var fakeUser3 = new IdentityUser()
+                {
+                    Id = "fake-003",
+                    UserName = "fake3@training.local"
+                };
+                var fake3Items = await service.GetIncompleteItemsAsync(fakeUser3);
+                Assert.Empty(fake3Items);
+            }
+        }
     }
 }
