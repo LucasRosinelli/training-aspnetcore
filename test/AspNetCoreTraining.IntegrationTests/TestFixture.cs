bc69516ab927ff61079b88607a0f2f65744874b2
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-25 10:23:22 +0100

    Test fixture; To-Do Route Should created.

---
+++ test/AspNetCoreTraining.IntegrationTests/TestFixture.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace AspNetCoreTraining.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),
+                        "..\\..\\..\\..\\..\\AspNetCoreTraining"));
+
+                    config.AddJsonFile("appsettings.json");
+                });
+
+            this._server = new TestServer(builder);
+
+            this.Client = this._server.CreateClient();
+            this.Client.BaseAddress = new Uri("http://localhost:8888");
+        }
+
+        public void Dispose()
+        {
+            this.Client.Dispose();
+            this._server.Dispose();
+        }
+    }
+}
---
+++ test/AspNetCoreTraining.IntegrationTests/ToDoRouteShould.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using Xunit;
+
+namespace AspNetCoreTraining.IntegrationTests
+{
+    public class ToDoRouteShould : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+
+        public ToDoRouteShould(TestFixture fixture)
+        {
+            this._client = fixture.Client;
+        }
+    }
+}
